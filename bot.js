8c24f9655b9b006b1818eae5a37d8d1884151e58
Petr Markov <channelpitnik@gmail.com>
2020-09-21 01:10:55 +0300

    emoji try 21

--- bot.js
+++ bot.js
@@ -11,6 +11,11 @@ client.on('ready', () => {
 client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
+    
+    const muteAll = (arg) => {
+        let channel = message.member.voiceChannel;
+        for (let member of channel.members) {member[1].setMute(arg)}
+    };
 
     if(command === 'помощь' || command=== 'help') message.reply('Я русский бот если что, так что разговаривай со мной по-русски\n'+
         '1) !привет -> здороваюсь в ответ\n'+
@@ -20,12 +25,10 @@ client.on('message', message => {
     if(command === 'привет') message.reply('приветствую вас!');
     if(command === 'любовь') message.channel.send('▄██▄██▄░██░░░▄█▀▀█▄░█▌░░▐█░██▀▀▀░▄██▄██▄\n▀█████▀░██░░▐█▌░░▐█▌▐█░░█▌░██▀▀░░▀█████▀\n ░░▀█▀░░░██▄▄░▀█▄▄█▀░░▀██▀░░██▄▄▄░░░▀█');
     if(command === 'цыц') {
-            let channel = message.member.voiceChannel;
-            for (let member of channel.members) {member[1].setMute(true)}
+            muteAll(true);
     }   
     if(command === 'алло') {
-            let channel = message.member.voiceChannel;
-            for (let member of channel.members) {member[1].setMute(false)}
+            muteAll(false);
     }
     if(command === 'поиграем'){
         message.reply('давайте! Напишите номер комнаты!');
@@ -40,9 +43,25 @@ client.on('message', message => {
                     message.channel
                         .send(`Номер комнаты: ${collected.first().content}\nОтреагируйте на мое сообщение ✅ для вкл микро и ❎ для выкл микро соотв`)
                         .then( function (gameMessage) {
-                            gameMessage.react('✅').then(r => {
-                                message.react('❎');
+                            gameMessage.react(':arrow_forward:').then(() => {
+                                gameMessage.react(':loud_sound:');
                             });
+                            let counter = 0;
+                            while (true){
+                                gameMessage.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == ':arrow_forward:'),
+                                { max: 1, time: 600000 })
+                                .then(collected => {
+                                    if (collected.first().emoji.name == ':arrow_forward:') {
+                                        counter++;
+                                        (counter % 2 === 0) ? muteAll(true) : muteAll(false);
+                                        reaction.users.remove(user.id);
+                                    }
+                                })
+                                .catch(() => {
+                                    break;
+                        
+                            });
+                            }
 
 
                         });
