8a308dc5fafffe84a7dd0cbae8a585750473a17c
Petr Markov <channelpitnik@gmail.com>
2020-09-21 01:18:45 +0300

    emoji try 24

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     
-    function muteAll(arg) {
+    function muteAll(channel, arg) {
         let channel = message.member.voiceChannel;
         for (let member of channel.members) {member[1].setMute(arg)}
     };
@@ -30,48 +30,48 @@ client.on('message', message => {
     if(command === 'алло') {
             muteAll(false);
     }
-    if(command === 'поиграем'){
-        message.reply('давайте! Напишите номер комнаты!');
-        message.channel.awaitMessages(m => m.author.id == message.author.id, {max: 1, time: 30000})
-                .then(collected => {
+    // if(command === 'поиграем'){
+    //     message.reply('давайте! Напишите номер комнаты!');
+    //     message.channel.awaitMessages(m => m.author.id == message.author.id, {max: 1, time: 30000})
+    //             .then(collected => {
             
-                    message.reply(`спасибо! Ваш номер: ${collected.first().content}`);
-                    message.channel.send('3');
-                    message.channel.send('2');
-                    message.channel.send('1');
-                    message.channel.send('ИГРА НАЧАЛАСЬ!');
-                    message.channel
-                        .send(`Номер комнаты: ${collected.first().content}\nКликни на эмодзи PLAY для вкл/откл звука`)
-                        .then( function (gameMessage) {
-                            gameMessage.react(':arrow_forward:').then(() => {
-                                gameMessage.react(':loud_sound:');
-                            });
-                            let counter = 0;
-                            while (true){
-                                gameMessage.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == ':arrow_forward:'),
-                                { max: 1, time: 600000 })
-                                .then(collected => {
-                                    if (collected.first().emoji.name == ':arrow_forward:') {
-                                        counter++;
-                                        (counter % 2 === 0) ? muteAll(false) : muteAll(true);
-                                        (counter % 2 === 0) ? gameMessage.react(':loud_sound:') : gameMessage.react(':mute:');
-                                        reaction.users.remove(user.id);
-                                    }
-                                })
-                                .catch(() => {
-                                    break;
+    //                 message.reply(`спасибо! Ваш номер: ${collected.first().content}`);
+    //                 message.channel.send('3');
+    //                 message.channel.send('2');
+    //                 message.channel.send('1');
+    //                 message.channel.send('ИГРА НАЧАЛАСЬ!');
+    //                 message.channel
+    //                     .send(`Номер комнаты: ${collected.first().content}\nКликни на эмодзи PLAY для вкл/откл звука`)
+    //                     .then( function (gameMessage) {
+    //                         gameMessage.react(':arrow_forward:').then(() => {
+    //                             gameMessage.react(':loud_sound:');
+    //                         });
+    //                         let counter = 0;
+    //                         while (true){
+    //                             gameMessage.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == ':arrow_forward:'),
+    //                             { max: 1, time: 600000 })
+    //                             .then(collected => {
+    //                                 if (collected.first().emoji.name == ':arrow_forward:') {
+    //                                     counter++;
+    //                                     (counter % 2 === 0) ? muteAll(false) : muteAll(true);
+    //                                     (counter % 2 === 0) ? gameMessage.react(':loud_sound:') : gameMessage.react(':mute:');
+    //                                     reaction.users.remove(user.id);
+    //                                 }
+    //                             })
+    //                             .catch(() => {
+    //                                 break;
                         
-                            });
-                            }
+    //                         });
+    //                         }
 
 
-                        });
+    //                     });
 
 
-                }).catch (() => {
-                    message.reply('извините, но 30 секунд прошло, а ответа я так и не дождался(');
-                });
-    }
+    //             }).catch (() => {
+    //                 message.reply('извините, но 30 секунд прошло, а ответа я так и не дождался(');
+    //             });
+    // }
 });
 
 
